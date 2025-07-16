8f370416e80ab476208fb2c9498e35da54e90ec6
joeafify <joe.afify95@gmail.com>
2025-07-16 13:04:47 +0300

    solution: function

--- topics/function/exercises/src/lib.rs
+++ topics/function/exercises/src/lib.rs
@@ -1,7 +1,7 @@
-pub fn mul() {
-    todo!();
+pub fn mul(num1: i32, num2: i32) -> i32 {
+    return num1 * num2;
 }
 
-pub fn div() {
-    todo!();
+pub fn div(num1: i32, num2: i32) -> i32 {
+    return num1 / num2;
 }
