ec7ad209aeb86014a43b798425b079a053cae19e
joeafify <joe.afify95@gmail.com>
2025-07-17 14:31:45 +0300

    solution: hash map

--- topics/hash_map/exercises/src/lib.rs
+++ topics/hash_map/exercises/src/lib.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
 pub fn init(address: String, amount: u32) -> HashMap<String, u32> {
-    todo!();
+    let mut hash: HashMap<String,u32> = HashMap::new();
+    hash.insert(address, amount);
+    return hash;
 }
