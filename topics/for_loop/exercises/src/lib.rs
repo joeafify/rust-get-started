d9cb7cc7d5087db75fa8fa17b0c89981da3656d0
joeafify <joe.afify95@gmail.com>
2025-07-17 15:23:52 +0300

    solution: for loop

--- topics/for_loop/exercises/src/lib.rs
+++ topics/for_loop/exercises/src/lib.rs
@@ -1,7 +1,15 @@
 pub fn sum(nums: Vec<i32>) -> i32 {
-    todo!();
+    let mut sum = 0;
+    for i in nums.iter() {
+        sum += i
+    }
+    sum
 }
 
 pub fn fill(i: u32, n: usize) -> Vec<u32> {
-    todo!();
+    let mut v:Vec<u32> = Vec::new();
+    for _ in 0..n  {
+        v.push(i);
+    }
+    v
 }
